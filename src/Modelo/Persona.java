7953a1d56d09fe53ba864d2af7f629dcec4c01a9
Javierms15 <Javiimartin15@uma.es>
2022-01-21 13:04:06 +0100

    persona acabada

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -3,39 +3,50 @@ package Modelo;
 import java.util.Date;
 
 public class Persona {
-    private String nombre;
+    private String primernombre;
     private String segundoNombre;
-    private String apellido;
+    private String primerapellido;
     private String segundoApellido;
     private Date fechaNacimiento;
-    private String dni;
+    private String nif;
+    private String  contrasena;
+    private String rcontrasena;
+    private String calle;
+    private int numero;
+    private String planta;
+    private String region;
+    private String ciudad;
+    private int CP;
+    private String Pais;
+    private boolean Valida;
 
-    public Persona(String nombre, String segundoNombre, String apellido, String segundoApellido, Date fechaNacimiento, String dni) {
-        this.nombre = nombre;
-        this.segundoNombre = segundoNombre;
-        this.apellido = apellido;
-        this.segundoApellido = segundoApellido;
-        this.fechaNacimiento = fechaNacimiento;
-        this.dni = dni;
+
+    public Persona(String n, String pn, String sn, String pa, String sa, Date f, String c, String rc, String ca, int num, String p, String r, String city, int cp, String pais, boolean v ){
+        this.nif=n;
+        this.primernombre=pn;
+        this.segundoNombre=null;
+        this.primerapellido=pa;
+        this.segundoApellido=null;
+        this.fechaNacimiento=f;
+        this.contrasena=c;
+        this.rcontrasena=rc;
+        this.calle=ca;
+        this.numero=num;
+        this.planta=p;
+        this.region=null;
+        this.ciudad=city;
+        this.CP=cp;
+        this.Pais=pais;
+        this.Valida=false;
+
     }
 
-    public Persona(String dni){
-        DBaccess acceso = new DBaccess();
-        Persona persona = acceso.buscarPersonas("dni", dni).get(0);
-        this.nombre = persona.getNombre();
-        this.segundoNombre = persona.getSegundoNombre();
-        this.apellido = persona.getApellido();
-        this.segundoApellido = persona.getSegundoApellido();
-        this.fechaNacimiento = persona.getFechanaciMiento();
-        this.dni = persona.getDni();
+    public String getPrimernombre() {
+        return primernombre;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setPrimernombre(String primernombre) {
+        this.primernombre = primernombre;
     }
 
     public String getSegundoNombre() {
@@ -46,12 +57,12 @@ public class Persona {
         this.segundoNombre = segundoNombre;
     }
 
-    public String getApellido() {
-        return apellido;
+    public String getPrimerapellido() {
+        return primerapellido;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public void setPrimerapellido(String primerapellido) {
+        this.primerapellido = primerapellido;
     }
 
     public String getSegundoApellido() {
@@ -62,19 +73,113 @@ public class Persona {
         this.segundoApellido = segundoApellido;
     }
 
-    public Date getFechanaciMiento() {
+    public Date getFechaNacimiento() {
         return fechaNacimiento;
     }
 
-    public void setFechanaciMiento(Date fechanaciMiento) {
-        this.fechaNacimiento = fechanaciMiento;
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 
-    public String getDni() {
-        return dni;
+    public String getNif() {
+        return nif;
     }
 
-    public void setDNI(String dni) {
-        this.dni = dni;
+    public void setNif(String nif) {
+        this.nif = nif;
     }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRcontrasena() {
+        return rcontrasena;
+    }
+
+    public void setRcontrasena(String rcontrasena) {
+        this.rcontrasena = rcontrasena;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(String planta) {
+        this.planta = planta;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getCP() {
+        return CP;
+    }
+
+    public void setCP(int CP) {
+        this.CP = CP;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public boolean isValida() {
+        return Valida;
+    }
+
+    public void setValida(boolean valida) {
+        Valida = valida;
+    }
+
+
+/*
+    public Persona(String dni){
+        DBaccess acceso = new DBaccess();
+        Persona persona = acceso.buscarPersonas("dni", dni).get(0);
+        this.nombre = persona.getNombre();
+        this.segundoNombre = persona.getSegundoNombre();
+        this.apellido = persona.getApellido();
+        this.segundoApellido = persona.getSegundoApellido();
+        this.fechaNacimiento = persona.getFechanaciMiento();
+        this.dni = persona.getDni();
+    }
+*/
 }
