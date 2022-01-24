351f06bde76b47cf7144cfc9f8e2d9512bc91e1d
rubenlopran <79511066+rubenlopran@users.noreply.github.com>
2022-01-24 20:08:51 +0100

    Cambio fecha

--- src/Modelo/DBaccess.java
+++ src/Modelo/DBaccess.java
@@ -66,7 +66,8 @@ public class DBaccess {
     public void crearCuentaPersona(String n, String pn, String sn, String pa, String sa, Date f, String c, String rc, String ca,
                                    int num, String p, String r, String city, int cp, String pais, boolean v, Persona.tipoP tp) throws Exception {
 //f.compareTo(null)==0 || tp.compareTo(null)==0
-        if(n.compareTo("")==0 || pn.compareTo("")==0||pa.compareTo("")==0  || c.compareTo("")==0 || rc.compareTo("")==0 || ca.compareTo("")==0 || num == 0 || p.compareTo("")==0 || city.compareTo("")==0 || pais.compareTo("")==0 || cp==0) {
+        Date fechaact = new Date();
+        if(n.compareTo("")==0 || pn.compareTo("")==0||pa.compareTo("")==0  || c.compareTo("")==0 || rc.compareTo("")==0 || ca.compareTo("")==0 || num == 0 || p.compareTo("")==0 || city.compareTo("")==0 || pais.compareTo("")==0 || cp==0 || f.compareTo(fechaact)==0) {
             throw new Exception("ERR1");
         }
         else if(c.compareTo(rc)!=0){
